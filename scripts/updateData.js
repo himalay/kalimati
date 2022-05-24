53a97da4db712c9f5d228a0c4dc4068d8102db87
Himalay Sunuwar <contact@himalay.com.np>
2022-05-24 21:44:42 +0545

    Revert module type

--- scripts/updateData.js
+++ scripts/updateData.js
@@ -1,35 +1,38 @@
-import axios from 'axios'
-import { wrapper } from 'axios-cookiejar-support'
-import { CookieJar } from 'tough-cookie'
-import { parse } from 'node-html-parser'
-import { writeFileSync } from 'fs'
+/* eslint-disable @typescript-eslint/no-var-requires */
+const axios = require('axios')
+const { wrapper } = require('axios-cookiejar-support')
+const { CookieJar } = require('tough-cookie')
+const { parse } = require('node-html-parser')
+const { writeFileSync } = require('fs')
 
 const axiosWithCookie = wrapper(axios)
 
 const cookieJar = new CookieJar()
 
-await axiosWithCookie.get('https://kalimatimarket.gov.np/lang/en', {
-  jar: cookieJar,
-  withCredentials: true,
-})
+axiosWithCookie
+  .get('https://kalimatimarket.gov.np/lang/en', {
+    jar: cookieJar,
+    withCredentials: true,
+  })
+  .then(({ data }) => {
+    axiosWithCookie.get('https://kalimatimarket.gov.np', {
+      jar: cookieJar,
+      withCredentials: true,
+    })
 
-const { data } = await axiosWithCookie.get('https://kalimatimarket.gov.np', {
-  jar: cookieJar,
-  withCredentials: true,
-})
+    const parsePrice = (price) => parseInt(price.replace('Rs.', '').trim())
 
-const parsePrice = (price) => parseInt(price.replace('Rs.', '').trim())
+    const parseRow = (row) => {
+      const [commodity, unit, min, max] = row.textContent.split(/\s{2,}/).filter((x) => !!x.trim())
 
-const parseRow = (row) => {
-  const [commodity, unit, min, max] = row.textContent.split(/\s{2,}/).filter(x => !!x.trim())
+      return { commodity, unit, min: parsePrice(min), max: parsePrice(max) }
+    }
 
-  return { commodity, unit, min: parsePrice(min), max: parsePrice(max) }
-}
+    const document = parse(data)
+    const dailyPriceTable = document.querySelector('#commodityPricesDailyTable')
+    const date = dailyPriceTable.querySelector('h5').textContent.split('-').pop().replace('A.D.', '').trim()
+    const headers = ['Commodity', 'Price (Rs)']
+    const rows = dailyPriceTable.querySelectorAll('table tr:not(:first-child)').map(parseRow)
 
-const document = parse(data)
-const dailyPriceTable = document.querySelector('#commodityPricesDailyTable')
-const date = dailyPriceTable.querySelector('h5').textContent.split('-').pop().replace('A.D.', '').trim()
-const headers = ['Commodity', 'Price (Rs)']
-const rows = dailyPriceTable.querySelectorAll('table tr:not(:first-child)').map(parseRow)
-
-writeFileSync('public/data.json', JSON.stringify({ date, headers, rows }))
+    writeFileSync('public/data.json', JSON.stringify({ date, headers, rows }))
+  })
